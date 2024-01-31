7d3b9b63bb93dc3aef99f8e033fb7d74c06ada43
namastegit <akshaykailey1507@gmail.com>
2024-01-31 18:15:29 +0530

    --

--- week4/01-DomBackendlogic.js
+++ week4/01-DomBackendlogic.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require("cors"); // Import the cors middleware npm install cors
+
 const app = express();
 app.use(bodyParser.json());
+app.use(cors()); // Enable CORS for all routes -- error occurs when a web page hosted on one domain tries to make a request to a different domain using JavaScript, and the server on the different domain doesn't include the necessary CORS headers to allow the request.
 
 app.get("/getsum", (req, res) => {
     const num1 = parseInt(req.query.num1);
