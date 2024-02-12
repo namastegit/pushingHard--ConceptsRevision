49e6e14cf55d8aa5f1af68d889be0a01d24e83ad
namastegit <akshaykailey1507@gmail.com>
2024-02-12 23:54:03 +0530

    --

--- week5/firstproject-R/src/App.jsx
+++ week5/firstproject-R/src/App.jsx
@@ -1,3 +1,24 @@
+// import { useState } from 'react'
+
+// import './App.css'
+
+// function App() {
+//   const [count, setCount] = useState(0)
+//   function incCounter() { 
+//     setCount(count + 1);
+//    }
+//   return (
+//  <div>
+// <button onClick={incCounter}>
+//           count is {count}
+//         </button>
+//  </div>
+  
+//   )
+// }
+
+// export default App
+
 import { useState } from 'react';
 import './App.css';
 
