8127b917801decfd9d0c451f370979bbfc7df203
namastegit <akshaykailey1507@gmail.com>
2024-02-16 17:17:58 +0530

    --

--- week5/FULLTODO/backend/db.js
+++ week5/FULLTODO/backend/db.js
@@ -1,8 +1,16 @@
 const mongoose = require("mongoose");
+const { boolean } = require("zod");
 
 mongoose.connect();
 
 const todoSchema = mongoose.Schema({
     title:String,
-    description:String
+    description:String,
+    completed: Boolean
 });
+const todo = mongoose.model('todosTable', todoSchema
+);
+
+module.exports = {
+    todo
+}
