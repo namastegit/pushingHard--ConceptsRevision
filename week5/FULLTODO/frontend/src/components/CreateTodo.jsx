0b2febaac3d4d0444267ee0093d16acee9aee550
namastegit <akshaykailey1507@gmail.com>
2024-02-16 18:33:25 +0530

    --

---
+++ week5/FULLTODO/frontend/src/components/CreateTodo.jsx
@@ -0,0 +1,11 @@
+export function CreateTodo() {
+    return <>
+    <div> <br></br>
+        <input type="text" placeholder="Title"></input> <br></br> <br></br>
+        <input type="text" placeholder="Description"></input> <br></br> <br></br>
+        <button>Add ToDo</button>
+    </div>
+    </>
+}
+
+// betweenthree21:dd3EI4DTIcYCj4A0@cluster0
