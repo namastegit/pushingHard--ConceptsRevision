3b8ac321a4dd9e9fc41b6be6a17bb398aaaebe88
namastegit <akshaykailey1507@gmail.com>
2024-02-17 13:43:29 +0530

    -

--- week5/FULLTODO/backend/db.js
+++ week5/FULLTODO/backend/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const { boolean } = require("zod");
 
-mongoose.connect("mongodb+srv://betweenthree21:dd3EI4DTIcYCj4A0@cluster0.svmm229.mongodb.net/TodoApp");
+mongoose.connect("");
 
 const todoSchema = mongoose.Schema({
     title:String,
