27c204664cccfcb6835a771903d5fa2d6d627ddf
namastegit <akshaykailey1507@gmail.com>
2024-02-21 22:51:52 +0530

    --

--- week6/w6-react/src/App.jsx
+++ week6/w6-react/src/App.jsx
@@ -1,9 +1,30 @@
+import { memo, useState } from 'react'
+import './App.css'
+
 function App() {
+  const [title, setTitle] = useState("hello2");
+  function addRandom() {
+setTitle(Math.random);
+  }
 
   return (
     <>
+    <div> <br></br> <br></br>
+      <button onClick={addRandom}>Add Random Number</button><br></br><br></br>
+      <Headers title = {title}></Headers><br></br>
+      <Headers title = "hello"></Headers><br></br>
+    </div>
     </>
   )
 }
+const Headers = memo(({title}) => {
+  return(
+    <>
+    <div>
+      {title}
+    </div>
+    </>
+  )
+})
 
 export default App
