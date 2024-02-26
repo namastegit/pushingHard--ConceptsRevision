58d6453ad65f5e4f7ec2e68f91e80fadcc9bf626
namastegit <akshaykailey1507@gmail.com>
2024-02-26 22:38:51 +0530

    -

--- week6/hooks-and-effects/hooksandeffects/src/App.jsx
+++ week6/hooks-and-effects/hooksandeffects/src/App.jsx
@@ -1,27 +1,31 @@
-import React, { useState } from "react";
+///6.2--  1:09:20 - another approach if dont want to use memoization -- by using useeffect[array] -- put expensive operation inside useEffect and [use this ] -- this is how we can control the rerendering of expensive operation. {harkirat only uses this approach only -- to see syntax got to video or camera pictures - date - 02/26/2024-- 6:15 PM}-- but for this approach we need to use one extra state variable(drawback)
+
+//2nd -- we can use useMemo also in place of memo and can take use of []-- usememo(()={},[])
+import React, { useState, memo} from "react";
 
 function App() {
+function ButtON() {
+  
   const [count, setCount] = useState(0);
 
   function counterINC() {
     setCount(count + 1);
   }
 
+
   function RefreshC() {
     setCount(0);
   }
-
-  return (
-    <>
-      <button onClick={counterINC}>Counter {count}</button>
+return (
+  <>
+  <button onClick={counterINC}>Counter {count}</button>
       <button onClick={RefreshC}>Refresh Counter</button> <br />
       <br />
-      <Component1 />
-    </>
-  );
+  </>
+)
 }
 
-function Component1() {
+const Component1 = memo(() => {
   const [value, setValue] = useState(0);
 
   return (
@@ -44,6 +48,15 @@ function Component1() {
       <br />
     </>
   );
+});
+
+  return (
+    <>
+      <ButtON/>
+      <Component1 />
+    </>
+  );
 }
+
 
 export default App;
