ae39b8743653e8dc42a04bd4ac5d0e4c592133b0
namastegit <akshaykailey1507@gmail.com>
2024-03-08 02:42:15 +0530

    init

--- week8/backendpaytm/db.js
+++ week8/backendpaytm/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 mongoose.connect("mongodb+srv://betweenthree21:dd3EI4DTIcYCj4A0@cluster0.svmm229.mongodb.net/paytmfrontend");
 
-const userSchema = mongoose.Schema({
+const userSchema = new mongoose.Schema({
 
     username: String,
     firstname: String,
@@ -9,8 +9,15 @@ const userSchema = mongoose.Schema({
     password: String,
 });
 
+const accountSchema = new mongoose.Schema({
+    userId: String, 
+    balance: Number,
+    token : String,
+});
 const User = mongoose.model("USER", userSchema);
+const Account = mongoose.model("ACCOUNT", accountSchema);
 
 module.exports ={
-User:User
+User:User,
+Account:Account,
 }
--- week8/backendpaytm/middlewares/authMiddleware.js
+++ week8/backendpaytm/middlewares/authMiddleware.js
@@ -8,19 +8,17 @@ async function authMiddleware(req, res, next) {
 
         const decodedToken = jwt.verify(token, JWT_SECRET);
 
-        if (decodedToken.id !== id) {
+        if (decodedToken.newUserid !== id) {
             res.json({
                 msg: "Invalid Credentials - Token"
             });
         } else {
-            req.id = decodedToken.id; 
+            req.userId = decodedToken.id;
             next();
         }
     } catch (error) {
         console.log(`Found Error: ${error}`);
-        res.status(500).json({
-            msg: "Server Error - Token"
-        });
+        res.status(500).json({ error: "Server Error" });
     }
 }
 
